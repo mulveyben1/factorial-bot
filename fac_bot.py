6b14d5efab9838e4d494e3df173b50f0163f07e7
Ben Mulvey <ben@mulveyfamily.com>
2017-12-01 21:41:27 -0500

    Increased size of number that can be handled

--- fac_bot.py
+++ fac_bot.py
@@ -12,15 +12,19 @@ async def on_ready():
     print (client.user.name)
     print(client.user.id)
     print('-----')
-    
+
 @client.event
 async def on_message(message):
     if message.content.find('!') != -1 and message.content[message.content.find('!')-1] != "@" :
         number = int(re.search('-?\d+', message.content).group())
         if message.content.find('-') != -1:
             await client.send_message(message.channel, "You can't take the factorial of a negative number!")
-        elif (number > 807):
-            await client.send_message(message.channel, 'That factorial is too large for Discord!')
+        elif (number > 15):
+            numberFac=math.factorial(number)
+            numberFac = "{:.5E}".format(numberFac)
+            await client.send_message(message.channel, numberFac)
+        elif (number >= 10000) :
+            await client.send_message(message.channel, "This factorial is too large to be computed in a reasonable amount of time!")
         else:
             numberFac=math.factorial(number)
             await client.send_message(message.channel, numberFac)
