a6a06e6629b53d3b8578f8f9eb22eaa40e784d9d
Ben Mulvey <ben@mulveyfamily.com>
2017-12-01 21:59:11 -0500

    mild improvements to large numbers

--- fac_bot.py
+++ fac_bot.py
@@ -24,6 +24,10 @@ async def on_message(message):
             numberFac=math.factorial(number)
             numberFac = "{:.5E}".format(Decimal(numberFac))
             await client.send_message(message.channel, numberFac)
+        elif (number > 5000):
+            await client.send_message(message.channel, "This might take a while.")
+            numberFac=math.factorial(number)
+            numberFac = "{:.5E}".format(Decimal(numberFac))
         elif (number >= 10000) :
             await client.send_message(message.channel, "This factorial is too large to be computed in a reasonable amount of time!")
         else:
