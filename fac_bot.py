50aaf3d03f1528cd23272ae63403b3396eb3271d
Ben Mulvey <ben@mulveyfamily.com>
2017-12-12 20:38:51 -0500

    More actual checking for factorials

--- fac_bot.py
+++ fac_bot.py
@@ -28,26 +28,34 @@ async def on_message(message):
         elif str(message.author) == "DJ Mitch#4397":
             print ("Not a factorial. (DJ Mitch.)")
         else:
-            number = int(re.search('-?\d+', message.content).group())
-            if message.content.find('-') != -1:
-                await client.send_message(message.channel, "You can't take the factorial of a negative number!")
-            elif (number >= 10000) :
-                await client.send_message(message.channel, "This factorial is too large to be computed in a reasonable amount of time!")
-            elif (number > 6000):
-                await client.send_message(message.channel, "This might take a while.")
-                numberFac = math.factorial(number)
-                numberFac = "{:.5E}".format(Decimal(numberFac))
-                await client.send_message(message.channel, numberFac)
-            elif (number > 15):
-                numberFac=math.factorial(number)
-                numberFac = "{:.5E}".format(Decimal(numberFac))
-                await client.send_message(message.channel, numberFac)
+            numberpre = None
+            try:
+                numberpre = re.search('-?\d+!', message.content).group()
+            except:
+                print("Not a factorial.")
+            if numberpre is None:
+                print("")
             else:
-                numberFac=math.factorial(number)
-                await client.send_message(message.channel, numberFac)
-            print (message.author)
-            print (message.content.find("!"))
-            print (message.content)
+                number = int(numberpre[:len(numberpre)-1])
+                if message.content.find('-') != -1:
+                    await client.send_message(message.channel, "You can't take the factorial of a negative number!")
+                elif (number >= 10000) :
+                    await client.send_message(message.channel, "This factorial is too large to be computed in a reasonable amount of time!")
+                elif (number > 6000):
+                    await client.send_message(message.channel, "This might take a while.")
+                    numberFac = math.factorial(number)
+                    numberFac = "{:.5E}".format(Decimal(numberFac))
+                    await client.send_message(message.channel, numberFac)
+                elif (number > 15):
+                    numberFac=math.factorial(number)
+                    numberFac = "{:.5E}".format(Decimal(numberFac))
+                    await client.send_message(message.channel, numberFac)
+                else:
+                    numberFac=math.factorial(number)
+                    await client.send_message(message.channel, numberFac)
+                    print (message.author)
+                    print (message.content.find("!"))
+                    print (message.content)
 
 
 client.run(conf.key)
